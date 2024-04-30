b8fad6a59c8c445687f787c6ad2c85c13c8c1086
Julian Verdurmen <304NotModified@users.noreply.github.com>
2024-04-30 23:31:42 +0200

    Added test case

---
+++ tests/NSubstitute.Acceptance.Specs/FieldReports/Issue725_AmbiguousMethodCall.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace NSubstitute.Acceptance.Specs.FieldReports;
+
+public class Issue725_AmbiguousMethodCall
+{
+    public interface ITest
+    {
+        Task<int?> TestAsync();
+    }
+
+    [Test]
+    public void Stub_any_string_and_call_with_null()
+    {
+        var test = Substitute.For<ITest>();
+        test.TestAsync().Returns(Task.FromResult(1));
+    }
+}
